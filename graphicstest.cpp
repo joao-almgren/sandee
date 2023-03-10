bd7a616423a4940f802e833228eb92bfc486cbcb
Joao Almgren <joao.almgren@ef.com>
2023-03-10 15:59:49 +0100

    Clean

--- graphicstest.cpp
+++ graphicstest.cpp
@@ -6,7 +6,7 @@ namespace
 {
 	using namespace DirectX;
 
-	const D3D11_INPUT_ELEMENT_DESC g_inputElementDesc[] =
+	const D3D11_INPUT_ELEMENT_DESC g_inputElementDesc[]
 	{
 		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
 		{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
@@ -16,8 +16,8 @@ namespace
 
 	struct Vertex
 	{
-		[[maybe_unused]] XMFLOAT3 position;
-		[[maybe_unused]] XMFLOAT4 color;
+		[[maybe_unused]] XMFLOAT3 position{};
+		[[maybe_unused]] XMFLOAT4 color{};
 	};
 	
 	const Vertex g_vertices[]
